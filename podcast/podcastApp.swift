a9cb29a4dbee06808500d5ff8d60e5b69cc696d4
Leesu <tndusg7@gmail.com>
2024-04-05 15:43:19 +0900

    Initial Commit

---
+++ podcast/podcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcastApp.swift
+//  podcast
+//
+//  Created by 박수연 on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct podcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
